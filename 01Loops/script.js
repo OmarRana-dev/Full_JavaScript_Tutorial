6a5fb175a82b5108d974d7e7f3e657ca5fde3c35
Omar Rana <110420500+OmarRana-dev@users.noreply.github.com>
2024-01-25 19:37:39 +0500

    push

---
+++ 01Loops/script.js
@@ -0,0 +1,88 @@
+// class by hary for for loop, for in loop, for of loop,
+/*
+let obj = {
+    talha: 30,
+    mahmoud: 40,
+    mohammed: 50,
+    risika: 60,
+    mamen: 90,
+}
+
+for (let a in obj) {
+    console.log(`The Current Object: ${a} are ${obj[a]}`);
+}
+
+let arr = ['Omar', 'Talha', 'Usman', 'Yaser', 'Chand', 'shari']
+for (let b of arr) {
+    console.log(`The Current Object: ${b}`);
+}
+*/
+
+// Exercise no 1.1
+
+/*
+let num1 = 30;
+for(let n = 0; n < num1; n++) {
+    if (n % 2 == 1) {
+        console.log(n);
+    }
+}
+*/
+
+
+// Exercise no 1.2
+
+/*
+let num2 = 12;
+for (let i = -14;num2 > i; num2--) {
+    if (num2 % 2 == 0) {
+            console.log(num2);
+        }
+}
+*/
+
+// Exercise no 1.3 
+
+/*
+let num3 = 50;
+for (let i = 20; num3 > i; num3--) {
+    if (num3 % 3 == 0) {
+        console.log(num3);
+    }
+
+}
+*/
+
+
+// Exercise no 2.1 
+/*
+let var2 = ['klsdf', 'omar', 'LC101', 'blue']
+for (let a in var2) {
+    console.log(var2[a]);
+}
+*/
+
+
+// Exercise no 2.2
+/*
+let var1 = "LaunchCode"
+for (let i = var1.length - 1; i >= 0; i--) { 
+    console.log(var1[i]); 
+} */
+
+
+// 2nd class by harry "for while loop" or "do while loop"
+
+// let n2 = 20;
+// let i = 10;
+// while (i < n2) {
+//     console.log(i+1);
+//     i++;
+// }
+
+// let n3 = 10;
+// let i = 3;
+// do {
+//     console.log(i+1)
+//     i++;
+// }while (i < n3);
