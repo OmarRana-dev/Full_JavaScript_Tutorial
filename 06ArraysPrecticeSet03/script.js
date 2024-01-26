0630c12839c445c33c57ffc6f1bb7ca5f3c1c681
Omar Rana <110420500+OmarRana-dev@users.noreply.github.com>
2024-01-27 00:13:48 +0500

    done first 3 prectices.

---
+++ 06ArraysPrecticeSet03/script.js
@@ -0,0 +1,17 @@
+// // 1. Write a JavaScript function to check whether an 'input' is and array or not.
+// function is_array(value) {
+//     return Array.isArray(value);
+// }
+// console.log(is_array("w3resource"));
+// console.log(is_array([1, 2, 3]));
+
+
+// // 2. Write a JavaScript function to clone an array.
+// function array_clone(value) {
+//     return value.slice(0);
+// }
+// console.log(array_clone([1, 2, 3, 0]))
+// console.log(array_clone([1, 2, [4, 5]]))
+
+
+// .3 Write a JavaScript function ot get the first element of an array. Passing the 'n' will return the first 'n' element of the array.
