85916247ed1b6c261221909150cc5b773131e662
Omar Rana <110420500+OmarRana-dev@users.noreply.github.com>
2024-01-28 19:10:54 +0500

    do some more numbers expressions and in prectice set 03 solve a problem

--- 05Number&Maths/script.js
+++ 05Number&Maths/script.js
@@ -1,6 +1,71 @@
-// toFixed() method word same as Math.round() do only the difference if it will return a string instead of return a number as Math.round() do. You will see Math.round() on next some lines
-let balance = 120.43;
-console.log(balance.toFixed())
+// Numbers
+
+// // --in javascript numbers are stored as a floating point numbers
+// console.log(23 === 23.0)
+
+// // Some Time using Number maybe makes error so be active when you are using decimal number
+// // JavaScript Stores number in 64 bits
+// console.log(0.2 + 0.1)
+// console.log((0.2 + 0.1) === 0.3)
+
+// // To solve the problem above, it helps to multiply and divide:
+// console.log((0.2 * 10 + 0.1 * 10) / 10)
+
+
+// // --Different ways of writting a number 
+// let billion = 1000000000;
+
+// // writting in this way maybe more readable
+// let billion1 = 1_000_000_000;
+// console.log(billion1)
+
+// // after e "n" number represent the "n" zero after 1
+// let billion2 = 1e9;
+// console.log(billion2)
+
+// // in this value -6 represent the numbers after decimel.  
+// let billion3 = 4354e-6;
+// console.log(billion3)
+
+
+// // --Writting numbers in different bases
+
+// // variable with binery number and console get it as a decimel number.
+// let x = 0b111111111;
+// console.log(x)
+
+// // variable with octal number and console get it as a decimel number
+// let y = 0o3455;
+// console.log(y)
+
+// // variable with hexadecimel number again console get it as a decimel number
+// let z = 0x4f3;
+// console.log(z)
+
+
+// // --Converting a number to string 
+// let num1 = 225;
+// console.log(num1.toString())
+
+// // We can also convert the base of the number with .toString()
+// // By default, JavaScript displays numbers as base 10 decimals.
+// // But you can use the toString() method to output numbers from base 2 to base 36.
+// // Hexadecimal is base 16. Decimal is base 10. Octal is base 8. Binary is base 2.
+// let num = 334;
+// console.log(`Hexatrigesimal (base 36): ${num.toString(36)}`); 
+// console.log(`Duotrigesimal (base 32): ${num.toString(32)}`); 
+// console.log(`Hexadecimal (base 16): ${num.toString(16)}`);
+// console.log(`Duodecimal (base 12): ${num.toString(12)}`); 
+// console.log(`Decimal (base 10): ${num.toString(10)}`); 
+// console.log(`Octal (base 8): ${num.toString(8)}`); 
+// console.log(`Binary (base 2): ${num.toString(2)}`); 
+
+
+
+
+// // toFixed() method word same as Math.round() do only the difference if it will return a string instead of return a number as Math.round() do. You will see Math.round() on next some lines
+// let balance = 120.43;
+// console.log(balance.toFixed())
 
 
 // // Format a number into a currency string, using the locale settings.
@@ -23,13 +88,6 @@ console.log(balance.toFixed())
 
 
 
-// // Some Time using Number maybe makes error so be active when you are using decimal number
-// let met1 = 0.2 * 7;
-// let met2 = 0.2 + 1.2;
-// // if ()
-// console.log(0.2 * 7)
-// console.log(0.2 + 1.2)
-// console.log(met1 === met2)
 
 // // Math.random() always returns a number lower than 1.
 // console.log(Math.random())
@@ -48,5 +106,6 @@ console.log(balance.toFixed())
 // let min = 20;
 // let max = 90;
 // console.log(Math.round(Math.random() * (max - min + 1)) + min) // Maybe this is deficult to understand so fro now just remember fromula or just look at this formula or when ever u need it then copy this formula from here. 
+
 
 
---
+++ 06PrecticeSet03/script.js
@@ -0,0 +1,16 @@
+// 1. Write a JavaScript function ot generate a random integer.
+function rand(min, max) {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+console.log(rand(20, 50)) //Done
+
+
+let minm = [3, 5, 6, 7, 8, 3, 5]
+
+let string = minm.join("");
+
+// console.log()
+// console.log(Math.max([2,3,5,677,2,34]))
+
+// console.log(Math.max(num))
