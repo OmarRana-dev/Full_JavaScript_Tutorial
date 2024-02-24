01ab61bb4fa2278f84e926db3c693a51b7d7e7d8
Omar Rana <110420500+OmarRana-dev@users.noreply.github.com>
2024-02-24 21:45:16 +0500

    modules

---
+++ 17Modules/script.js
@@ -0,0 +1,10 @@
+// import { sayHi, sayName } from "./utility.js";
+import * as utility from "./utility.js";
+
+console.log(utility.sayHi("Omar"))
+console.log(utility.sayName("Omar"))
+
+
+
+
+
---
+++ 17Modules/utility.js
@@ -0,0 +1,12 @@
+function sayHi(name) {
+    return `Hi ${name}!`
+}
+
+function sayName(name) {
+    return `Your Name is ${name}`;
+}
+
+export {
+    sayName,
+    sayHi
+}
