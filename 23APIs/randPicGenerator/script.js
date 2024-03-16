7f394cc2f8ccb59c87ea7ba52d68527fb7f04005
Omar Rana <110420500+OmarRana-dev@users.noreply.github.com>
2024-03-16 23:38:57 +0500

    update

--- 23APIs/randPicGenerator/script.js
+++ 23APIs/randPicGenerator/script.js
@@ -1,27 +1,7 @@
 const img = document.querySelector("img");
-// const input = document.querySelector("input");
-// const btn = document.querySelector("button");
-
-// btn.addEventListener("submit", () => {
-//   const value = input.value;
-//   fetch(
-//     `https://api.giphy.com/v1/gifs/translate?api_key=bEcOMA02AQjfm9J8dGzli3qaZaUIxp4D&s=${value}`,
-//     { mode: "cors" }
-//   )
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((response) => {
-//       img.src = response.data.images.original.url;
-//       console.log(response);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// });
 
 fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=bEcOMA02AQjfm9J8dGzli3qaZaUIxp4D&s=cars",
+  "https://api.giphy.com/v1/gifs/translate?api_key=bEcOMA02AQjfm9J8dGzli3qaZaUIxp4D&s=iron man",
   { mode: "cors" }
 )
   .then((response) => {
@@ -34,4 +14,3 @@ fetch(
   .catch((error) => {
     console.log(error);
   });
-
