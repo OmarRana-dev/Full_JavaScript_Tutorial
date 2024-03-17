35366080fee75bd4540012a954f8649ee8b7ef3e
Omar Rana <110420500+OmarRana-dev@users.noreply.github.com>
2024-03-17 19:43:58 +0500

    complete api project

--- 23APIs/randPicGenerator/script.js
+++ 23APIs/randPicGenerator/script.js
@@ -1,16 +1,33 @@
 const img = document.querySelector("img");
+const url =
+  "https://api.giphy.com/v1/gifs/translate?api_key=bEcOMA02AQjfm9J8dGzli3qaZaUIxp4D&s=";
 
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=bEcOMA02AQjfm9J8dGzli3qaZaUIxp4D&s=iron man",
-  { mode: "cors" }
-)
-  .then((response) => {
-    return response.json();
-  })
-  .then((response) => {
-    img.src = response.data.images.original.url;
-    console.log(response);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+const formEl = document.querySelector(".form");
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const input = document.querySelector("input").value;
+  const url1 = url + input;
+
+  fetch(url1)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      appendChild(response.data.images.original.url);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+const bodyEl = document.querySelector("body");
+function appendChild(url) {
+  const child = document.querySelector("img");
+  bodyEl.removeChild(child);
+
+  const img = document.createElement("img");
+  img.setAttribute("src", `${url}`);
+
+  bodyEl.appendChild(img);
+}
