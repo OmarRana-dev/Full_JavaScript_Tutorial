1e3741a39b4436cdb6957e4e72a5985b06a8cde5
Omar Rana <110420500+OmarRana-dev@users.noreply.github.com>
2024-03-26 21:54:34 +0500

    update

--- 23APIs/api.js
+++ 23APIs/api.js
@@ -1,16 +1,25 @@
-const lastUpdate = document.querySelector("#lastUpdate");
-
 fetch(
-  "https://api.weatherapi.com/v1/current.json?key=989e8d4d83124f15a0772835241603&q=hafizabad"
+  "https://api.weatherapi.com/v1/forecast.json?key=989e8d4d83124f15a0772835241603&q=Lahore&days=3"
 )
   .then((resolve) => {
+    console.log(resolve.status);
     return resolve.json();
   })
   .then((data) => {
     console.log(data);
-    lastUpdate.textContent = data.current.last_updated;
-
+    const iconUrl = data.current.condition.icon;
+    console.log(iconUrl);
+    dayORnight(iconUrl);
   })
   .catch((reject) => {
     console.log(reject);
   });
+
+
+
+
+function night(code) {
+
+
+
+}
