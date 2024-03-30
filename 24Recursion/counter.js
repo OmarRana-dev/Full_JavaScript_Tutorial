a0caf1e17a56137d3455b58af7e9eff3eca4d5fd
Omar Rana <110420500+OmarRana-dev@users.noreply.github.com>
2024-03-30 06:10:01 +0500

    project done

--- 24Recursion/counter.js
+++ 24Recursion/counter.js
@@ -123,8 +123,80 @@ function factorial(value) {
   console.log(`the current value: ${value}`);
   const total = value * factorial(value - 1);
   console.log(`now total is: ${total}`);
-  console.log(`N is: ${n}`);
   return total;
 }
 
 console.log(factorial(5));
+
+function power(num1, num2) {
+  let value = num1;
+  if (num2 === 0) {
+    return 1;
+  }
+
+  return value * power(num1, num2 - 1);
+}
+
+// console.log(power(2, 3));
+
+// Question: Product of an array
+// Write a function called productOfArray which takes in an array of numbers and returns the product of them all
+
+function productOfArray(arr) {
+  if (arr.length === 0) return 1;
+
+  return arr.shift() * productOfArray(arr);
+}
+
+// console.log(productOfArray([1, 2, 3, 10]));
+
+const nestedObject = {
+  data: {
+    info: {
+      stuff: {
+        thing: {
+          moreStuff: {
+            magicNumber: 44,
+            something: "foo2",
+          },
+        },
+      },
+    },
+  },
+};
+
+function contains(obj, value) {
+  for (let key in obj) {
+    if (typeof obj[key] === "object") {
+      return contains(obj[key], value);
+    }
+    if (obj[key] === value) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// console.log(contains(nestedObject, "foo2"));
+
+function all(array, callback) {
+  if (array.length === 0) return true;
+
+  if (callback(array[0])) {
+    array.shift();
+    return all(array, callback);
+  } else {
+    return false;
+  }
+}
+
+const arr = [2, 4, 1, 6];
+
+const response = all(arr, function (num) {
+  // console.log(num);
+  return num < 7;
+});
+// console.log(response);
+
+// Question: Search JS object
+// Write a function called contains that searches for a value in a nested object. It returns true if the object contains that value.
---
+++ 25fibonacciProject/fibonacci.js
@@ -0,0 +1,88 @@
+function fibs(num) {
+  const fibonacciSequence = [];
+
+  if (num === 0) return fibonacciSequence;
+  if (num === 1) {
+    fibonacciSequence.push(0);
+    return fibonacciSequence;
+  }
+
+  let i = 0;
+
+  let a = 0;
+  let b = 1;
+  while (i < num) {
+    fibonacciSequence.push(a);
+    const c = a + b;
+    a = b;
+    b = c;
+
+    i++;
+  }
+
+  return fibonacciSequence;
+}
+
+// console.log(fibs(9));
+
+function fibsRec(n) {
+  const fibonacciSequence = [];
+
+  let i = 0;
+  while (i < n) {
+    function fibonacci(n) {
+      if (n < 2) return n;
+      return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    fibonacciSequence.push(fibonacci(i));
+    i++;
+  }
+
+  return fibonacciSequence;
+}
+
+// console.log(fibsRec(9));
+
+function mergeSort(numArray) {
+  if (numArray.length <= 1) return numArray;
+
+  const middle = Math.floor(numArray.length / 2);
+
+  const leftHalf = numArray.slice(0, middle);
+  const rightHalf = numArray.slice(middle);
+
+  return merge(mergeSort(leftHalf), mergeSort(rightHalf));
+}
+
+function merge(left, right) {
+  const merged = [];
+  let i = 0;
+  let j = 0;
+
+  console.log(`Leftes: ${left} & Rightes: ${right}`);
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      merged.push(left[i]);
+      i++;
+    } else {
+      merged.push(right[j]);
+      j++;
+    }
+  }
+
+  while (i < left.length) {
+    merged.push(left[i]);
+    i++;
+  }
+  while (j < right.length) {
+    merged.push(right[j]);
+    j++;
+  }
+
+  console.log(`Merged: ${merged}`);
+  return merged;
+}
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1, 23]));
