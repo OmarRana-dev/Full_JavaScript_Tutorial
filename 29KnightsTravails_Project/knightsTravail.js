939ceaacdda7b3059e1069a9a36749b17767e703
Omar Rana <110420500+OmarRana-dev@users.noreply.github.com>
2024-04-25 23:38:07 +0500

    not done yet i will do this project some time later

--- 29KnightsTravails_Project/knightsTravail.js
+++ 29KnightsTravails_Project/knightsTravail.js
@@ -1,6 +1,60 @@
-const knightMoves = [
-  [0, 0],
-  [1, 2],
-];
+// Example chessboard size (8x8)
+const BOARD_SIZE = 8;
 
-console.log(knightMoves);
+function generateMoves(row, col) {
+  const moves = [];
+  const deltas = [
+    [-2, -1],
+    [-1, -2],
+    [1, -2],
+    [2, -1],
+    [2, 1],
+    [1, 2],
+    [-1, 2],
+    [-2, 1],
+  ];
+
+  for (const [dr, dc] of deltas) {
+    const newRow = row + dr;
+    const newCol = col + dc;
+
+    if (isValidSquare(newRow, newCol)) {
+      moves.push([newRow, newCol]);
+    }
+  }
+
+  return moves;
+}
+
+function isValidSquare(row, col) {
+  return row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE;
+}
+
+function knightMoves(start, end) {
+  const queue = [[start]];
+  const visited = new Set([start]);
+
+  while (queue.length > 0) {
+    const path = queue.shift();
+    const [row, col] = path[path.length - 1];
+
+    if (row === end[0] && col === end[1]) {
+      return path;
+    }
+
+    const moves = generateMoves(row, col);
+
+    for (const move of moves) {
+      if (!visited.has(move)) {
+        queue.push([...path, move]);
+        visited.add(move);
+      }
+    }
+  }
+
+  // If no path is found
+  return null;
+}
+
+console.log(knightMoves([0, 0], [7, 7])); // Output: [[0,0],[1,2]]
+console.log(knightMoves([0, 0], [3, 3])); // Output: [[0,0],[2,1],[3,3]]
