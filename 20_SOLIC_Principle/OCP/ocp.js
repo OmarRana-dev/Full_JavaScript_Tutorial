4b72782fa94e9b378ff1fd6cc9b47830adfbea97
Omar Rana <110420500+OmarRana-dev@users.noreply.github.com>
2024-05-27 17:00:04 +0500

    OCP done

---
+++ 20_SOLIC_Principle/OCP/ocp.js
@@ -0,0 +1,23 @@
+// Open-close principle
+class PaymentProcessor {
+  constructor(paymentProcessor) {
+    this.processor = paymentProcessor;
+  }
+  processPayment(amount) {
+    this.processor.processPayment(amount);
+  }
+}
+class CreditCardProcessor {
+  processPayment(amount) {
+    console.log(`Processing credit card payment of ${amount}`);
+  }
+}
+class PaypalProcessor {
+  processPayment(amount) {
+    console.log(`Processing paypal payment of ${amount}`);
+  }
+}
+const creditCardProcessor = new CreditCardProcessor();
+const paypalProcessor = new PaypalProcessor();
+const processor = new PaymentProcessor(paypalProcessor);
+processor.processPayment(100);
